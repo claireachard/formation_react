7b6c72b6c45ee485dbc21e00abd6cfbb6a42c83f
claireachard <achardclaire@yahoo.fr>
2021-09-13 13:14:24 +0100

    first button

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button/Button'
 
 function App() {
   return (
     <div className="App">
       Helloworld
+      <hr/>
+      <Button/>
     </div>
   );
 }
---
+++ src/App/components/Button/Button.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Button.css'
+
+const Button=()=>{
+    return (
+        <button className="button" >Benjamin</button>
+    );
+}
+
+export default Button;
