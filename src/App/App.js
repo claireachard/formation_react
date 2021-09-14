1ee4e88d8bb32e9cffbcf65cde863871e51faced
claireachard <achardclaire@yahoo.fr>
2021-09-14 15:39:32 +0100

    prepare tchat

--- src/App/App.js
+++ src/App/App.js
@@ -1,33 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './App.css'
+import Tchat from './components/Tchat/Tchat';
 
-import './App.css';
-import Button from './components/Button/Button';
-
-class App extends React.Component {
-
-  constructor(props){
-    super(props);
-    this.state={counter:0};
-  }
-
+class App extends Component {
   render() {
     return (
       <div className="App">
-        Valeur du compteur: {this.state.counter}
-
-        <Button onClickEvent={()=>{
-          // this.state.counter--;
-          this.setState({counter:this.state.counter-1})
-          console.log(this.state.counter);
-        }}>soustraction</Button>
-        <Button onClickEvent={()=>{
-          // this.state.counter++;
-          this.setState({counter:this.state.counter+1})
-          console.log(this.state.counter);
-        }}
-        bgColor="chartreuse">addition</Button>
+        <Tchat/>
       </div>
-    )
+    );
   }
 }
 
